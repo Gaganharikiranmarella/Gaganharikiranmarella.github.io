6301365755025b196399b3428c4058c724d18941
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-13 12:55:03 +0530

    modified script.js and style.css

--- web1/script.js
+++ web1/script.js
@@ -1,18 +1,18 @@
-function showSection(section) {
-  const greenSection = document.getElementById('greenSection');
-  const cookedSection = document.getElementById('cookedSection');
-  const greenBtn = document.getElementById('greenBtn');
-  const cookedBtn = document.getElementById('cookedBtn');
+  function showSection(section) {
+    const greenSection = document.getElementById('greenSection');
+    const cookedSection = document.getElementById('cookedSection');
+    const greenBtn = document.getElementById('greenBtn');
+    const cookedBtn = document.getElementById('cookedBtn');
 
-  if (section === 'green') {
-    greenSection.style.display = 'block';
-    cookedSection.style.display = 'none';
-    greenBtn.classList.add('active');
-    cookedBtn.classList.remove('active');
-  } else {
-    greenSection.style.display = 'none';
-    cookedSection.style.display = 'block';
-    cookedBtn.classList.add('active');
-    greenBtn.classList.remove('active');
+    if (section === 'green') {
+      greenSection.style.display = 'block';
+      cookedSection.style.display = 'none';
+      greenBtn.classList.add('active');
+      cookedBtn.classList.remove('active');
+    } else {
+      greenSection.style.display = 'none';
+      cookedSection.style.display = 'block';
+      greenBtn.classList.remove('active');
+      cookedBtn.classList.add('active');
+    }
   }
-}
