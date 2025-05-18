894d03087a999683566fbc2925b77a962885e857
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-18 17:40:21 +0530

    reduce method added to a16

--- a16/index.js
+++ a16/index.js
@@ -1,4 +1,4 @@
-const products = [
+/*const products = [
   { id: 1, name: "Apple", price: 50 },
   { id: 2, name: "Mango", price: 60 },
   { id: 3, name: "Banana", price: 80 }
@@ -11,4 +11,25 @@ products.map(product => {
     const total = product.price * cart[product.id];
     console.log(product.id, product.name, product.price, total);
   }
-});
+});*/
+
+const products = [
+  { id: 1, name: "Apple", price: 50 },
+  { id: 2, name: "Mango", price: 60 },
+  { id: 3, name: "Banana", price: 80 }
+];
+
+const cart = { 1: 7, 2: 9 };
+
+const totalOrder = products.reduce((sum, product) => {
+  if (cart[product.id]) {
+    const qty = cart[product.id];
+    const total = product.price * qty;
+    console.log(product.name, product.price, qty, total);
+    return sum + total;
+  }
+  return sum;
+}, 0);
+
+console.log("Total Order Value:", totalOrder);
+
